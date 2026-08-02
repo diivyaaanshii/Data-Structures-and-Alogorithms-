ae37e5f43a99704132bd0c60ebc39b64aa96f5b4
Divyanshi Diwan <diivyaaanshii@gmail.com>
2026-08-02 12:38:41 +0530

    Time: 11 ms (12.01%) | Memory: 12.8 MB (44.90%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -7,13 +7,11 @@ class Solution(object):
         """
         i=0
         j=len(numbers)-1
-        while(i<j):
+        while i<j:
             sum=numbers[i]+numbers[j]
             if sum==target:
                 return [i+1,j+1]
-            elif sum<=target:
-                i+=1
-            else:
+            elif sum>target:
                 j-=1
-        
-        
+            else:
+                i+=1
