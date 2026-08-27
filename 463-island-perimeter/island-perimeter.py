25a7a2f717aa11a73d93edc2cb6c84950768f22e
Divyanshi Diwan <diivyaaanshii@gmail.com>
2026-08-28 02:58:40 +0530

    Time: 39 ms (73.28%) | Memory: 12.7 MB (44.35%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -9,8 +9,8 @@ class Solution(object):
         row=len(grid)
         col=len(grid[0])
         p=0
-        for r  in xrange(row):
-            for c in xrange(col):
+        for r in xrange(row):
+            for c in range(col):
                 if grid[r][c]==1:
                     p+=4
                     if r>0 and grid[r-1][c]==1:
@@ -18,4 +18,3 @@ class Solution(object):
                     if c>0 and grid[r][c-1]==1:
                         p-=2
         return p
-            
